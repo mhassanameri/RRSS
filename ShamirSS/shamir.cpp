2691776ca01142aa4edc49667b42c8b2764b8052
mhassanameri <m.hassan.ameri@gmail.com>
2026-07-22 11:27:09 -0400

    Shamir with Arb GF_2E
    Working on initial function description without debugging.

--- ShamirSS/shamir.cpp
+++ ShamirSS/shamir.cpp
@@ -15,6 +15,9 @@ typedef vector<vector<point>> shares;
 scheme::scheme(int members,int threshold){
     n = members;
     k = threshold;
+    GF_2E_degree =  smallest_lambda1_for_2n(n); // for example, when n = 128, we have GF_2E_degree = 9, and we get  GF_2^9
+    init_GF2E_field(GF_2E_degree);
+
 }
 
 /*Creating of shares shamir (n,k) scheme*/
@@ -133,4 +136,71 @@ vec_ZZ_p scheme::createShares_NTL_p(ZZ_p secret) {
 
 
     return allShares;
+}
+
+
+void scheme::init_GF2E_field(long lambda1) {
+    GF2X P;
+    BuildIrred(P, lambda1);   // irreducible polynomial of degree lambda1
+    GF2E::init(P);
+    // field_degree = lambda1;
+}
+
+
+
+vec_GF2E scheme::createShares_GF2E(const GF2E& secret, long numShares, long threshold) {
+    if (numShares > ((1L << GF_2E_degree) - 1)) {
+        throw std::runtime_error("Not enough nonzero field elements for Shamir evaluation points.");
+    }
+
+    vec_GF2E allShares;
+    allShares.SetLength(numShares);
+
+    vec_GF2E coeff;
+    coeff.SetLength(threshold);
+
+    coeff[0] = secret;
+    for (long j = 1; j < threshold; j++) {
+        coeff[j] = random_GF2E();   // uniform in GF(2^lambda1)
+    }
+
+    for (long i = 0; i < numShares; i++) {
+        GF2E x = ulong_to_GF2E(i + 1); // distinct nonzero elements
+        GF2E y;
+        clear(y);
+
+        GF2E xpow;
+        set(xpow); // xpow = 1
+
+        for (long j = 0; j < threshold; j++) {
+            y += coeff[j] * xpow;
+            xpow *= x;
+        }
+
+        allShares[i] = y;
+    }
+
+    return allShares;
+}
+
+long scheme::smallest_lambda1_for_2n(long n) {
+    if (n <= 0) {
+        throw std::invalid_argument("n must be positive");
+    }
+
+    unsigned long long target = 2ULL * static_cast<unsigned long long>(n);
+
+    long lambda1 = 0;
+    unsigned long long field_size = 1ULL; // 2^lambda1
+
+    while (field_size <= target) {        // need 2^lambda1 > 2n
+        if (field_size > (std::numeric_limits<unsigned long long>::max() >> 1)) {
+            throw std::overflow_error("n is too large for unsigned long long");
+        }
+
+        field_size <<= 1;                 // multiply by 2
+        lambda1++;
+    }
+
+    return lambda1;
 }
--- ShamirSS/shamir.h
+++ ShamirSS/shamir.h
@@ -5,6 +5,10 @@
 
 #include <NTL/ZZ_pEX.h>
 #include <NTL/LLL.h>
+#include <NTL/GF2E.h>
+#include <NTL/GF2X.h>
+#include <NTL/vec_GF2E.h>
+#include <NTL/GF2XFactoring.h>
 using namespace NTL;
 
 namespace shamir{
@@ -23,24 +27,40 @@ typedef vec_ZZ shares_NTL;
 
   class scheme{
     int n,k;
+    long GF_2E_degree;
   public:
     typedef struct point_p{
       ZZ_p x;
       ZZ_p y;
     }point_p;
+
+    typedef struct point_GF2E {
+      GF2E x;
+      GF2E y;
+    } point_GF2E;
+
     scheme(int members,int threshold);
     shares* createShares(std::string secret);
     shares_NTL* createShares_NTL(std::string secret);
     // shares_NTL_p* createShares_NTL_p(ZZ_p secret);
     vec_ZZ_p createShares_NTL_p(ZZ_p secret);
+    vec_GF2E createShares_GF2E(const GF2E& secret, long numShares, long threshold);
 
     std::string getSecret(shares* Kshares);
+    /*
+   * Some useful wrapper functions:
+   */
+    long smallest_lambda1_for_2n(long n);
+    void init_GF2E_field(long lambda1);
+
   };
   /*
   this function should be called whenever the shamir library is required to used
   it calls the function gen_multipletable and srand for rand function.
   */
   void init();
+
+
 }
 
 #endif
